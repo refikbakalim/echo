74fff5c75fd8f253cc00357c521d37f06c2c7fb7
refikbakalim <refikbakalim@gmail.com>
2021-03-12 00:31:38 +0300

    init project

---
+++ webapp/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html") 
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    message = request.form["usermsg"]
+    if message == '':
+        message = "Empty Message"
+    return message
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
